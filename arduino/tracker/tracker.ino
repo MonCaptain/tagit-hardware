26ebb8d53e68c696a7d8aac492219ead6cca7751
shehabmohsen <aquashehab612@gmaiil.com>
2024-12-09 14:50:13 -0500

    feat: update trcacker file with latest iteration

--- arduino/tracker/tracker.ino
+++ arduino/tracker/tracker.ino
@@ -1,33 +1,19 @@
-#define TINY_GSM_MODEM_SIM7600 //needs to be defined before including TINYGsmClient
+
+#define TINY_GSM_MODEM_SIM7600
+
 #define TINY_GSM_RX_BUFFER 1024 // Set RX buffer to 1Kb
 
-#include "WiFi.h" // to connect to wifi
-#include <TinyGsmClient.h> // for GPS module
+#include <TinyGsmClient.h>
 #include "secrets.h" // to get network name and password
-#include <ArduinoMqttClient.h>
-#include <WiFiClientSecure.h>
-// Function prototypes
+#include <PubSubClient.h>
 
-String getGPSString();
-String getGPSString2();
-void connectWifi();
-void connectAWS();
-void enableGPSPower();
-void disableGPSPower();
-
-// Wifi and password
-const char* ssid = SECRET_SSID;
-const char* password =  SECRET_PASS;
-// MQTT publisher / subscriber
-const String topic = SECRET_MQTT_TOPIC;
-const String BUS_NAME = SECRET_CLIENT_DEVICE;
-
-// LilyGO T-SIM7600G Pinout
+// LilyGO T-SIM7000G Pinout
 #define UART_BAUD   115200
 #define PIN_DTR     25
 #define PIN_TX      27
 #define PIN_RX      26
 #define PWR_PIN     4
+
 #define LED_PIN     12
 
 // Set serial for debug console (to Serial Monitor, default speed 115200)
@@ -35,18 +21,27 @@ const String BUS_NAME = SECRET_CLIENT_DEVICE;
 // Set serial for AT commands
 #define SerialAT  Serial1
 
-// Serial communication for GPS
-TinyGsm modem(SerialAT); 
-// WifiClient to establish MQTT connection 
-WiFiClientSecure wifiClient = WiFiClientSecure();
-MqttClient mqttClient(wifiClient);
+// function prototypes 
+boolean mqttConnect();
+String getGPSString();
+void enableGPSPower();
+void disableGPSPower();
+String gpsTestString();
 
-void setup() {
+// initialize GPRS modem and mqtt client
+TinyGsm modem(SerialAT);  
+TinyGsmClient client(modem);
+PubSubClient mqtt(client);
+const char APN[32] = SECRET_CELLULAR_APN;
+
+void setup(){
   SerialMon.begin(115200);
+  SerialMon.println("Place your board outside to catch satelite signal");
 
   // Set LED OFF
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
+
   //Turn on the modem
   pinMode(PWR_PIN, OUTPUT);
   digitalWrite(PWR_PIN, HIGH);
@@ -54,19 +49,16 @@ void setup() {
   digitalWrite(PWR_PIN, LOW);
 
   delay(1000);
-  connectWifi();
-  connectAWS();
   
   // Set module baud rate and UART pins
   SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
-
   // Restart takes quite some time
   // To skip it, call init() instead of restart()
   SerialMon.println("Initializing modem...");
   if (!modem.restart()) {
     Serial.println("Failed to restart modem, attempting to continue without restarting");
   }
-  
+  delay(1000);
   // Print modem info
   String modemName = modem.getModemName();
   delay(500);
@@ -75,21 +67,51 @@ void setup() {
   String modemInfo = modem.getModemInfo();
   delay(500);
   SerialMon.println("Modem Info: " + modemInfo);
+
+  if (!modem.gprsConnect(APN)){
+    SerialMon.println(("FAILED to connect gprs"));
+  }
+  else {
+    SerialMon.println(("gprs successfully connected"));
+  }
+  delay(1000);
+  // Set SIM7000G GPIO4 HIGH ,turn on GPS power
+  // CMD:AT+SGPIO=0,4,1,1
+  // Only in version 20200415 is there a function to control GPS power
+  modem.sendAT("+SGPIO=0,4,1,1");
+  if (modem.waitResponse(10000L) != 1) {
+    SerialMon.println(" SGPIO=0,4,1,1 false ");
+  }
+  modem.enableGPS();
+  delay(15000);
+  // test cellular connectivity
+  SerialMon.println("Checking for cellular connectivity...");
+  if (modem.waitForNetwork(30000)) { // wait up to 30 seconds for network
+    SerialMon.println("Connected to cellular network!");
+    int16_t signal = modem.getSignalQuality();
+    SerialMon.print("Signal Quality: ");
+    SerialMon.println(signal);
+    
+    if (modem.localIP()> 0) {
+      SerialMon.print("IP Address:");
+      SerialMon.println(modem.localIP());
+    } else {
+      SerialMon.println("Failed to obtain IP address.");
+    }
+  } else {
+    SerialMon.println("Failed to connect to cellular network.");
+  }
+  mqtt.setServer("broker.emqx.io", 1883);
+  delay(500);
+  mqttConnect();
 }
 
 void loop() {
-  
-  // get GPS coordinates
   modem.maintain();
-  String coordinatesString = getGPSString();
-  // String coordinatesString = "this is test coordinate string";
-  // send coordinates in the form of "latitude,longitude"   
-  mqttClient.beginMessage(topic);
-  SerialMon.println(coordinatesString);
-  mqttClient.print(coordinatesString);
-  mqttClient.endMessage();
-  
-  delay(5000);
+  delay(15000);
+  String coordinatesJson = getGPSString();   
+  SerialMon.println(coordinatesJson);
+  mqtt.publish(SECRET_MQTT_TOPIC, coordinatesJson.c_str());
 }
 
 void enableGPSPower() {
@@ -118,9 +140,9 @@ String getGPSString() {
   int hour = 0, min = 0, sec = 0;
   
   char jsonString[256];  // Buffer to hold final JSON
-  String busName = BUS_NAME;
+  String busName = SECRET_CLIENT_DEVICE;
 
-  for (int8_t i = 5; i; i--) {
+  for (int8_t i = 15; i; i--) {
     SerialMon.println("Requesting current GPS location");
     
     if (modem.getGPS(&lat, &lon, &speed, &alt, &vsat, &usat, &accuracy, &year, &month, &day, &hour, &min, &sec)) {
@@ -128,10 +150,10 @@ String getGPSString() {
       break;
     } else {
       SerialMon.println("Failed to get GPS data, retrying...");
-      delay(5000L);  // Retry after 5 seconds
+      delay(15000L);  // Retry after 5 seconds
       
      if (i == 1) { 
-        disableGPSPower();  
+        // disableGPSPower();  
         sprintf(jsonString, "{\"message\": {\"error\": \"could not retrieve GPS coordinates for %s\"}}", busName.c_str());
         return jsonString;
       }
@@ -142,7 +164,7 @@ String getGPSString() {
   sprintf(jsonString, "{\"message\": \"{\\\"name\\\": \\\"%s\\\", \\\"latitude\\\": \\\"%.8f\\\", \\\"longitude\\\": \\\"%.8f\\\"}\"}", 
           busName.c_str(), lat, lon);
 
-  disableGPSPower();  // Disable GPS power after use
+  // disableGPSPower();  // Disable GPS power after use
 
   // Print the final JSON string
   SerialMon.println(jsonString);
@@ -150,31 +172,27 @@ String getGPSString() {
   return jsonString;
 }
 
+boolean mqttConnect() {
+  SerialMon.print("Connecting to ");
+  SerialMon.print(MQTT_BROKER_ENDPOINT);
 
-void connectWifi() {
-  WiFi.begin(ssid, password);
+  boolean status = mqtt.connect(SECRET_CLIENT_DEVICE);
 
-  while (WiFi.status() != WL_CONNECTED){
-    delay(500);
-    SerialMon.println("connecting to wifi...\n");
+  if (status == false) {
+    SerialMon.println(" fail");
+    return false;
   }
-  SerialMon.println("Wifi Connected!!!\n");
-  SerialMon.println("IP Address:");
-  SerialMon.println(WiFi.localIP());
+  SerialMon.println("success");
+  mqtt.publish(SECRET_MQTT_TOPIC, "shuttle1 ready");
+  mqtt.subscribe(SECRET_MQTT_TOPIC);
+  return mqtt.connected();
 }
 
-void connectAWS(){
-  const String msg = "Hello World!";
-
-  wifiClient.setCACert(AWS_CERT_CA);
-  wifiClient.setCertificate(AWS_CERT_CRT);
-  wifiClient.setPrivateKey(AWS_CERT_PRIVATE);
-
-  if (mqttClient.connect(SECRET_AWS_ENDPOINT, 8883)) {
-    SerialMon.println("You're connected to the MQTT Broker!\n");
-  }
-  else {
-    SerialMon.println("Failed to connect to MQTT Broker!\n");
-    SerialMon.println(mqttClient.connectError());
-  }
+String gpsTestString() {
+  char jsonString[256];
+  String busName = SECRET_CLIENT_DEVICE;
+  float lat = 40.813540, lon = -73.955286;
+  sprintf(jsonString, "{\"message\": \"{\\\"name\\\": \\\"%s\\\", \\\"latitude\\\": \\\"%.8f\\\", \\\"longitude\\\": \\\"%.8f\\\"}\"}", 
+          busName.c_str(), lat, lon);
+  return jsonString;
 }
